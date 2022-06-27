62605aa946ea15fa2ff481365f1b20bdbf0e449e
NHTanim <nazmulhossaintanim78@gmail.com>
2022-06-27 13:00:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+// event show
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+    let text =  document.getElementById('text');
+    text.innerHTML = ('My name is Tanim');
+    console.log('click');
+});
